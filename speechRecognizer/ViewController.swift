4ce5cf04f3e500264aada5c41e7b1103a20d8e24
佐藤一成 <kazunarisato0309@gmail.com>
2020-06-05 03:14:40 +0900

    Initial Commit

---
+++ speechRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speechRecognizer
+//
+//  Created by 佐藤一成 on 2020/06/05.
+//  Copyright © 2020 s140. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
